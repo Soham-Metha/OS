11e6c66ccad6351f9c499aa32a3d83dce522be46
Soham Metha <sohammetha01@gmail.com>
2026-01-19 20:33:13 +0530

    Update main function

--- shell/shell.c
+++ shell/shell.c
@@ -1,12 +1,13 @@
 #include "shell.h"
 
-void printHello()
-{
-    printf("\nHello World!\n\n");
-}
-
 int main(void)
 {
-    printHello();
+    uint8 c = load(0);
+    for (size_t i = 0; i < 15; i++)
+        store(1, '=');
+
+    printf("\nc = 0x%.02hhx = %c", c, c);
+    printf("\nerrno = 0x%.02x\n", errno);
+
     return 0;
 }
