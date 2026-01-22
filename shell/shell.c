414801a0d04dfc5b1d9375e52da951ebfd831146
Soham Metha <sohammetha01@gmail.com>
2026-01-23 01:44:10 +0530

    Main page refactor

--- shell/shell.c
+++ shell/shell.c
@@ -1,16 +1,22 @@
 #include "shell.h"
+#include <hal/hal.h>
+#include <hal/interrupt.h>
+
+#define COL(r, g, b, a) (r << 24 | g << 16 | b << 8 | a)
 
 int main(void)
 {
+    tty t = { 0 };
+    tty_init(&t, 225, 55, COL(0xFF, 0xFF, 0xFF, 0xFF), COL(0, 0, 0xFF, 0xFF));
+    tty_write_char(&t, 'H');
+    tty_write_char(&t, 'i');
+    tty_write_char(&t, '!');
+    tty_write_char(&t, '\n');
 
-    store(1, 'H');
-    store(1, 'i');
-    store(1, '!');
-    store(1, '\n');
+    for (uint8 i = 0; i < 225; i++)
+        tty_write_char(&t, '=');
 
-    for (uint8 i = 0; i < 15; i++)
-        store(1, '=');
+    tty_write_char(&t, '\n');
 
-    store(1, '\n');
     return 0;
 }
