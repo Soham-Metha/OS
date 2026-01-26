e49ba6dd7e90c531387a3cbd3a1d36ccd5e234b2
Soham Metha <sohammetha01@gmail.com>
2026-01-27 00:42:18 +0530

    native: implement interrupts

--- arch/native/boot.c
+++ arch/native/boot.c
@@ -1,69 +1,85 @@
-#include "../hal.h"
-#include <common/types.h>
+#include "boot.h"
+#include <kernel/interrupt.h>
 
-extern void gdtFlush(uint32);
-extern int main(void);
-extern void kernel_tick(void);
-
-typedef struct descriptor {
-    uint16 limit_00_15;
-    uint16 base_00_15;
-    uint8 base_16_23;
-    uint8 AccessByte;
-    uint8 limit_16_19_flags;
-    uint8 base_24_31;
-} __attribute__((packed)) Descriptor;
-
-typedef struct gdtPtr {
-    uint16 limit;
-    uint32 base;
-} __attribute__((packed)) GdtPtr;
-
-struct multiboot_info {
-    uint32 flags;         // indicates which fields are valid
-    uint32 mem_lower;     // valid if flags[0] = 1
-    uint32 mem_upper;
-    uint32 boot_device;     // flags[1]
-    uint32 cmdline;         // flags[2]
-    uint32 mods_count;      // flags[3]
-    uint32 mods_addr;
-    uint32 syms[4];         // flags[4] & [5] (a.out or ELF)
-    uint32 mmap_length;     // flags[6]
-    uint32 mmap_addr;
-    uint32 drives_length;     // flags[7]
-    uint32 drives_addr;
-    uint32 config_table;         // flags[8]
-    uint32 boot_loader_name;     // flags[9]
-    uint32 apm_table;            // flags[10]
-    uint32 vbe_control_info;     // flags[11]
-    uint32 vbe_mode_info;
-    uint16 vbe_mode;
-    uint16 vbe_interface_seg;
-    uint16 vbe_interface_off;
-    uint16 vbe_interface_len;
-    // Framebuffer info (flags[12])
-    uint64 framebuffer_addr;       // physical address of linear framebuffer
-    uint32 framebuffer_pitch;      // bytes per scanline
-    uint32 framebuffer_width;      // pixels
-    uint32 framebuffer_height;     // pixels
-    uint8 framebuffer_bpp;         // bits per pixel
-    uint8 framebuffer_type;        // 0 = indexed, 1 = RGB, 2 = EGA text
-    uint8 framebuffer_red_field_position;
-    uint8 framebuffer_red_mask_size;
-    uint8 framebuffer_green_field_position;
-    uint8 framebuffer_green_mask_size;
-    uint8 framebuffer_blue_field_position;
-    uint8 framebuffer_blue_mask_size;
-    uint8 reserved[6];     // padding to align struct
-};
-
-Descriptor gdtEntries[5];
-GdtPtr gdtPtr;
+IDT_Entry idt[256];
+GDT_Entry gdtEntries[5];
+static bool vectors[256];
 
 static uint32* fb;
 static uint32 pitch;
 static uint32 width;
 static uint32 height;
+
+static inline uint8 inb(uint16 port)
+{
+    uint8 ret;
+    __asm__ volatile("inb %1, %0"
+        : "=a"(ret)
+        : "Nd"(port));
+    return ret;
+}
+
+static inline void outb(uint16 port, uint8 value)
+{
+    __asm__ volatile("outb %0, %1"
+        :
+        : "a"(value), "Nd"(port));
+}
+
+// https://wiki.osdev.org/8259_PIC
+void pic_remap(uint8 offset1, uint8 offset2)
+{
+    /* initialization sequence */
+    outb(PIC1_COMMAND, 0x11);
+    outb(PIC2_COMMAND, 0x11);
+
+    /* vector offsets */
+    outb(PIC1_DATA, offset1);
+    outb(PIC2_DATA, offset2);
+
+    /* tell Master PIC there is a slave at IRQ2 (0000 0100) */
+    outb(PIC1_DATA, 0x04);
+    /* tell Slave PIC its cascade identity (0000 0010) */
+    outb(PIC2_DATA, 0x02);
+
+    /* set 8086/88 mode */
+    outb(PIC1_DATA, 0x01);
+    outb(PIC2_DATA, 0x01);
+
+    outb(PIC1_DATA, 0);
+    outb(PIC2_DATA, 0);
+}
+
+void pic_send_eoi(uint8 irq)
+{
+    if (irq >= 8)
+        outb(PIC2_COMMAND, PIC_EOI);
+
+    outb(PIC1_COMMAND, PIC_EOI);
+}
+
+// timer
+void irq0_handler_c(void)
+{
+    kernel_tick();
+    pic_send_eoi(0);
+}
+
+// keyboard
+void irq1_handler_c(void)
+{
+    uint8 scan_code = inb(0x60);
+    if (scan_code < 128)
+        kernel_irq(IRQ_KEYBOARD, scan_code_ascii[scan_code]);
+    pic_send_eoi(1);
+}
+
+void exception_handler(uint32 vector, uint32 error_code)
+{
+    (void)vector;
+    (void)error_code;
+    __asm__ volatile("cli; hlt");     // !! hangs the computer
+}
 
 void setGdtEntry(uint32 num, uint32 base, uint32 limit, uint8 access, uint8 flags)
 {
@@ -78,18 +94,53 @@ void setGdtEntry(uint32 num, uint32 base, uint32 limit, uint8 access, uint8 flag
     gdtEntries[num].AccessByte = access;
 }
 
+void setIdtEntry(uint8 vector, void* isr, uint8 flags)
+{
+    idt[vector].isr_low    = (uint32)isr & 0xFFFF;
+    idt[vector].kernel_cs  = 0x08;
+    idt[vector].attributes = flags;
+    idt[vector].isr_high   = (uint32)isr >> 16;
+    idt[vector].reserved   = 0;
+}
+
 extern void kernelMain(uint32 magic, struct multiboot_info* mbi)
 {
+    Descriptor_Ptr gdtPtr, idtr;
     (void)magic;
-    gdtPtr.limit = (sizeof(Descriptor) * 5) - 1;
+    gdtPtr.limit = (sizeof(GDT_Entry) * 5) - 1;
     gdtPtr.base  = (uint32)&gdtEntries;
 
-    setGdtEntry(0, 0, 0, 0, 0);
+    setGdtEntry(0, 0, 0x00000000, 0x00, 0x00);
     setGdtEntry(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
     setGdtEntry(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
     setGdtEntry(3, 0, 0xFFFFFFFF, 0xFA, 0xCF);
     setGdtEntry(4, 0, 0xFFFFFFFF, 0xF2, 0xCF);
     gdtFlush((uint32)&gdtPtr);
+
+    idtr.limit = (sizeof(IDT_Entry) * 256) - 1;
+    idtr.base  = (uint32)&idt;
+
+    for (uint8 i = 0; i < 32; i++) {
+        setIdtEntry(i, isr_stub_table[i], 0x8E);
+        vectors[i] = true;
+    }
+
+    pic_remap(0x20, 0x28);
+
+    /* mask everything */
+    outb(0x21, 0xFF);
+    outb(0xA1, 0xFF);
+
+    /* unmask (IRQ0, IRQ1) */
+    outb(0x21, 0xFc);
+
+    setIdtEntry(32, (void*)(uint32)isr_stub_32, 0x8E);
+    setIdtEntry(33, (void*)(uint32)isr_stub_33, 0x8E);
+    vectors[32] = true;
+    vectors[33] = true;
+
+    __asm__ volatile("lidt %0" : : "m"(idtr));
+    __asm__ volatile("sti");
 
     fb     = (uint32*)(uint32)mbi->framebuffer_addr;
     pitch  = mbi->framebuffer_pitch / 4;
@@ -97,9 +148,6 @@ extern void kernelMain(uint32 magic, struct multiboot_info* mbi)
     height = mbi->framebuffer_height;
 
     (void)main();
-    (void)kernel_tick();
-    // TODO: kernel tick should be periodically called
-    // TODO: keyboard interrupt handler
 }
 
 void hal_put_pixel(int x, int y, uint32 rgba)
@@ -114,7 +162,7 @@ void hal_clear(uint32 rgba)
     (void)rgba;
     for (uint32 y = 0; y < height; y++)
         for (uint32 x = 0; x < width; x++)
-            fb[y * pitch + x] =  rgba >> 8;
+            fb[y * pitch + x] = rgba >> 8;
 }
 
 void hal_present(void)
