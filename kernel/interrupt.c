ca058645fae3942685cfeb47f8f0f9f6063ce7b2
Soham Metha <sohammetha01@gmail.com>
2026-01-28 01:41:19 +0530

    interrupt: fix boundary violation

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -1,16 +1,17 @@
 #include "interrupt.h"
 #include "event.h"
-// TODO: fix boundary violation
-#include <userspace/shell.h>
+#include "scheduler.h"
 
 void kernel_irq(Interrupt i, uint64 data)
 {
     switch (i) {
     case IRQ_TIMER:
-        kernel_tick();     // TODO: should be a kernel function not a userspace one
+        resume(TASK_WAITING);
+        schedule();
         break;
     case IRQ_KEYBOARD:
         kernel_event_enque((Event) { .type = EVENT_KEYBOARD, .as.key_event.keycode = data });
+        resume(TASK_BLOCKED);
         break;
     case IRQ_COUNT:
     default:
