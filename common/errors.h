91f9647ce1967a896d0f6b98fc00af8de1eca535
Soham Metha <sohammetha01@gmail.com>
2026-02-01 00:28:24 +0530

    errors: cleanup

--- common/errors.h
+++ common/errors.h
@@ -4,26 +4,11 @@
 #define ERRORS_1
 
 typedef enum Error {
-    ERR_OK = 0,
     ERR_BAD_IO,
     ERR_INVALID_SYSCALL,
     ERR_BAD_FILE_DESC,
+    ERR_NO_INPUT_AVAILABLE,
     ERR_COUNT
 } Error;
-
-#define reterr(x)  \
-    do {           \
-        errno = x; \
-        return 0;  \
-    } while (false)
-
-#endif
-#ifdef IMPL_ERRORS_1
-
-Error errno;
-
-#else
-
-extern Error errno;
 
 #endif
