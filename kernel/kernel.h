063274dd8854c01bd23c4d0aa0ebcb8ac1e3a399
Soham Metha <sohammetha01@gmail.com>
2026-02-03 03:13:39 +0530

    kernel: remove global variable

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -9,16 +9,13 @@
 typedef enum Syscall {
     SYS_READ,
     SYS_WRITE,
+    // SYS_OPEN,
+    // SYS_CLOSE,
+    // SYS_CHMOD,
     SYS_YIELD,
     SYS_EXIT,
     SYS_COUNT,
 } Syscall;
-
-typedef struct Kernel {
-    tty* active_tty;
-} Kernel;
-
-extern Kernel k;
 
 Result8 __syscall_dispatch(Syscall s, uint64 a, uint64 b, uint64 c);
 
