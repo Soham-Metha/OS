781c462d37eb38221c7d56108956ebceb2a247ba
Soham Metha <sohammetha01@gmail.com>
2026-03-04 03:37:54 +0530

    types: add back the missing stdin and stdout defs

--- common/types.h
+++ common/types.h
@@ -22,6 +22,8 @@ typedef uint32 uintPtr;
 #define bool _Bool
 #define true 1
 #define false 0
+#define stdin 0
+#define stdout 1
 #define va_list __builtin_va_list
 #define va_start __builtin_va_start
 #define va_end __builtin_va_end
