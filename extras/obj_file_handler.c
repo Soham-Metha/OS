6fbd4e3e6d1e791e4f96e1888b9a06642def6db7
Soham Metha <sohammetha01@gmail.com>
2026-03-30 00:07:54 +0530

    scripts: update gfx handlers to get texture info (uv)

--- extras/obj_file_handler.c
+++ extras/obj_file_handler.c
@@ -6,20 +6,27 @@
 #define MAX_LINE 256
 
 typedef struct {
+    float u, v;
+} Point2f;
+
+typedef struct {
     float x, y, z, w;
 } Point3f;
 
 typedef struct {
     Point3f vertex[3];
     Point3f normal[3];
+    Point2f texture[3];
 } Tri3f;
 
 Point3f verts[MAX_VERTS];
 Point3f norms[MAX_VERTS];
+Point2f uvs[MAX_VERTS];
 
 int vert_count = 0;
 int norm_count = 0;
 int trin_count = 0;
+int uv_count   = 0;
 
 /**
  * Simple script to convert Obj files into static tables for 3d graphics!
@@ -28,7 +35,7 @@ int trin_count = 0;
 
 int main()
 {
-    FILE* f = fopen("tea.obj", "r");
+    FILE* f = fopen("fish.obj", "r");
     if (!f) {
         perror("open failed");
         return 1;
@@ -36,7 +43,7 @@ int main()
 
     char line[MAX_LINE];
 
-    printf("#define TEA_MESH {   \\\n");
+    printf("#define FISH_MESH {   \\\n");
 
     while (fgets(line, sizeof(line), f)) {
 
@@ -52,20 +59,27 @@ int main()
             sscanf(line, "vn %f %f %f", &n->x, &n->y, &n->z);
         }
 
+        // ---- tex ----
+        else if (strncmp(line, "vt ", 3) == 0) {
+            Point2f* t = &uvs[uv_count++];
+            sscanf(line, "vt %f %f", &t->u, &t->v);
+        }
+
         // ---- face ----
         else if (strncmp(line, "f ", 2) == 0) {
 
             int v[3]    = { 0 };
+            int vt[3]   = { -1, -1, -1 };
             int vn[3]   = { -1, -1, -1 };
 
             // Try: f v/vt/vn
             int matches = sscanf(line,
-                "f %d/%*d/%d %d/%*d/%d %d/%*d/%d",
-                &v[0], &vn[0],
-                &v[1], &vn[1],
-                &v[2], &vn[2]);
+                "f %d/%d/%d %d/%d/%d %d/%d/%d",
+                &v[0], &vt[0], &vn[0],
+                &v[1], &vt[1], &vn[1],
+                &v[2], &vt[2], &vn[2]);
 
-            if (matches != 6) {
+            if (matches != 9) {
                 // Try: f v//vn
                 matches = sscanf(line,
                     "f %d//%d %d//%d %d//%d",
@@ -75,16 +89,26 @@ int main()
             }
 
             if (matches < 6) {
-                // Fallback: f v v v
-                sscanf(line,
-                    "f %d %d %d",
-                    &v[0], &v[1], &v[2]);
+                matches = sscanf(line,
+                    "f %d/%d %d/%d %d/%d",
+                    &v[0], &vt[0],
+                    &v[1], &vt[1],
+                    &v[2], &vt[2]);
             }
 
             // Fetch vertices
             Point3f p[3];
             for (int i = 0; i < 3; i++) {
                 p[i] = verts[v[i] - 1];
+            }
+
+            // Fetch uv (or default 0)
+            Point2f t[3] = { 0 };
+            for (int i = 0; i < 3; i++) {
+                if (vt[i] != -1) {
+                    t[i]   = uvs[vt[i] - 1];
+                    t[i].v = 1.0f - t[i].v; // Blender user bottom-up texturig
+                }
             }
 
             // Fetch normals (or default 0)
@@ -102,6 +126,11 @@ int main()
                 "{ .x=%f, .y=%f, .z=%f, .w=1.0f },"
                 "{ .x=%f, .y=%f, .z=%f, .w=1.0f }"
                 "},"
+                "    .texture = {"
+                "{ .u=%f, .v=%f },"
+                "{ .u=%f, .v=%f },"
+                "{ .u=%f, .v=%f }"
+                "},"
                 // "    .normal = {"
                 // "{ .x=%f, .y=%f, .z=%f, .w=1.0f },"
                 // "{ .x=%f, .y=%f, .z=%f, .w=1.0f },"
@@ -111,7 +140,10 @@ int main()
                 trin_count,
                 p[0].x, p[0].y, p[0].z,
                 p[1].x, p[1].y, p[1].z,
-                p[2].x, p[2].y, p[2].z
+                p[2].x, p[2].y, p[2].z,
+                t[0].u, t[0].v,
+                t[1].u, t[1].v,
+                t[2].u, t[2].v
                 // n[0].x, n[0].y, n[0].z,
                 // n[1].x, n[1].y, n[1].z,
                 // n[2].x, n[2].y, n[2].z
--- extras/ppm_handler.c
+++ extras/ppm_handler.c
@@ -5,19 +5,9 @@
 typedef unsigned int uint32;
 #define COL(r, g, b, a) (r << 24 | g << 16 | b << 8 | a)
 
-static void skip_comments(FILE* f)
-{
-    int c;
-    while ((c = fgetc(f)) == '#') {
-        while (fgetc(f) != '\n')
-            ;
-    }
-    ungetc(c, f);
-}
-
 int main()
 {
-    FILE* f = fopen("pika.ppm", "rb");     // IMPORTANT: binary mode
+    FILE* f = fopen("fish_tex.ppm", "rb");     // IMPORTANT: binary mode
     if (!f) {
         perror("open failed");
         return 1;
@@ -29,14 +19,12 @@ int main()
         fprintf(stderr, "Only P6 supported\n");
         return 1;
     }
-    // skip_comments(f);
     int w, h, maxval;
     fscanf(f, "%d %d", &w, &h);
-    // skip_comments(f);
     fscanf(f, "%d", &maxval);
     fgetc(f);
 
-    printf("#define PIKA_TEST static uint32 img_px[%d] = { \\\n", w * h);
+    printf("#define FISH_PX static uint32 img_px[%d] = { \\\n", w * h);
 
     for (int i = 0; i < w * h; i++) {
         unsigned char rgb[3];
@@ -68,7 +56,7 @@ int main()
 
     printf("}\n\n");
 
-    printf("#define PIKA_CANVAS { \\\n");
+    printf("#define FISH_CANVAS { \\\n");
     printf("    .px = img_px,");
     printf("    .px_w = %d,", w);
     printf("    .px_h = %d,", h);
